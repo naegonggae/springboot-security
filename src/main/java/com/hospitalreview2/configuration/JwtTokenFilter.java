3c152a6362370b435b3c5e4d7df3a7841cda4c68
naegonggae <https://github.com/naegonggae/>
2022-12-19 22:36:25 +0900

    권한부여하기 ;문열어주기

---
+++ src/main/java/com/hospitalreview2/configuration/JwtTokenFilter.java
@@ -0,0 +1,38 @@
+package com.hospitalreview2.configuration;
+
+import com.hospitalreview2.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter { // OncePerRequestFilter는 입장할때마다 매번 티켓을 보여주는 방식
+
+    private final UserService userService;
+    private final String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // 권한 주거나 안주기 , 입구라고 생각하면 됨
+        // 개찰구 역할, 카드를 찍으면 열어주는 기능을 만들거임
+        // 현재는 모두 닫혀있습니다.
+
+        // 문열어주기
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("", null, List.of(new SimpleGrantedAuthority("USER"))    );
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        filterChain.doFilter(request, response);
+
+
+    }
+}
--- src/main/java/com/hospitalreview2/configuration/SecurityConfiguration.java
+++ src/main/java/com/hospitalreview2/configuration/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.hospitalreview2.configuration;
 
+import com.hospitalreview2.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,13 +11,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
     //Token을 확인하는 절차를 만들어보겠습니다.
     //JWT – Claim 좌석번호, 승객이름
     //UsernamePasswordAuthenticationFilter전에 TokenFilter를 구현해서 사용 합니다.
+
+    private final UserService userService;
+    @Value("${jwt.token.secret}") // .yml에서 토큰 가져와
+    private String secretKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -24,12 +34,13 @@ public class SecurityConfiguration {
                 .cors().and()
                 .authorizeRequests()
                 .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // join, login은 언제나 가능
-                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated() // 모든 post요청을 권한이 있어야 받게함 , 순서도 중요!
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()  // 문을 만들기 // 모든 post요청을 권한이 있어야 받게함 , 순서도 중요!
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
                 .and()
-                //.addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻 입니다.
+                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻 입니다.
+                // 토큰 필터는 티켓을 산거라고 보면 돼
                 .build();
     }
 }
