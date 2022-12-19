1cd0c2f5507c254c0365707021c7caba43763bb0
naegonggae <https://github.com/naegonggae/>
2022-12-20 00:10:36 +0900

    토큰 유효한지 체크하기

--- src/main/java/com/hospitalreview2/configuration/JwtTokenFilter.java
+++ src/main/java/com/hospitalreview2/configuration/JwtTokenFilter.java
@@ -1,7 +1,10 @@
 package com.hospitalreview2.configuration;
 
 import com.hospitalreview2.service.UserService;
+import com.hospitalreview2.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
@@ -18,6 +21,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenFilter extends OncePerRequestFilter { // OncePerRequestFilter는 입장할때마다 매번 티켓을 보여주는 방식
 
     private final UserService userService;
@@ -29,6 +33,37 @@ public class JwtTokenFilter extends OncePerRequestFilter { // OncePerRequestFilt
         // 권한 주거나 안주기 , 입구라고 생각하면 됨
         // 개찰구 역할, 카드를 찍으면 열어주는 기능을 만들거임
         // 현재는 모두 닫혀있습니다.
+
+        //언제 막아야 할까요?
+        //1. Token을 안가지고 온다. —> Request할 때 Token을 안넣고 호출하는 경우
+        //2. 적절하지 않은 Token을 가지고 온다      -> 이거는 어떤 부분에서 확인하는지 알수있을까요??
+        //3. 지난 Token을 가지고 온다
+
+        // 약간 토큰 값 String으로 뽑아내는 느낌 ;변수 선언
+        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorizationHeader:{}", authorizationHeader);
+
+        // token이 없으면 에러처리하기
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // token 분리
+        String token;
+        try {
+            token = authorizationHeader.split(" ")[1];
+        } catch (Exception e) { // 잘못된 토큰을 보냈는지
+            log.error("token 추출에 실패 했습니다.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Token이 만료되었는지 체크
+        if (JwtTokenUtil.isExpired(token, secretKey)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         // 문열어주기
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("", null, List.of(new SimpleGrantedAuthority("USER")));
--- src/main/java/com/hospitalreview2/utils/JwtTokenUtil.java
+++ src/main/java/com/hospitalreview2/utils/JwtTokenUtil.java
@@ -7,6 +7,17 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtTokenUtil {
+
+    private static Claims extractClaims(String token, String key) {
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+
+    public static boolean isExpired(String token, String secretKey) {
+        // expire timestamp를 return함
+        Date expiredDate = extractClaims(token, secretKey).getExpiration();
+        return expiredDate.before(new Date());
+    }
+
     public static String createToken(String userName, String key, long expireTimeMs) {
         Claims claims = Jwts.claims(); // 일종의 map
         claims.put("userName", userName);
